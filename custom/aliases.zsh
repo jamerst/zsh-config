dc6871df307d976c2b1aa3325bd548f33c8abe64
James Tattersall <10601770+jamerst@users.noreply.github.com>
2018-05-27 19:46:58 +0100

    Update aliases and add precmd

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,11 +1,16 @@
 
-# LS - REPLACES SOME IN ~/.oh-my-zsh/lib/directories.zsh
-alias l='exa -l --group-directories-first --time-style long-iso'
-alias la='exa -la --group-directories-first --time-style long-iso'
+# CHMOD
+alias chex='chmod u+x'
+alias ch644='chmod 644'
+alias ch755='chmod 755'
 
 # HOSTNAMES
 alias joshua='joshua.dcs.warwick.ac.uk'
 alias raw='radio.warwick.ac.uk'
+
+# LS - REPLACES SOME IN ~/.oh-my-zsh/lib/directories.zsh
+alias l='exa -l --group-directories-first --time-style long-iso'
+alias la='exa -la --group-directories-first --time-style long-iso'
 
 # SSH
 alias sshj='ssh u1708480@joshua.dcs.warwick.ac.uk'
