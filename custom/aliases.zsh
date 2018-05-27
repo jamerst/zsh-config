7468469a69c241cd377b485879de64f0cdba32e5
James Tattersall <10601770+jamerst@users.noreply.github.com>
2018-05-27 16:04:43 +0100

    Add aliases file

---
+++ custom/aliases.zsh
@@ -0,0 +1,12 @@
+
+# LS - REPLACES SOME IN ~/.oh-my-zsh/lib/directories.zsh
+alias l='exa -l --group-directories-first --time-style long-iso'
+alias la='exa -la --group-directories-first --time-style long-iso'
+
+# HOSTNAMES
+alias joshua='joshua.dcs.warwick.ac.uk'
+alias raw='radio.warwick.ac.uk'
+
+# SSH
+alias sshj='ssh u1708480@joshua.dcs.warwick.ac.uk'
+alias sshr='ssh jtattersall@radio.warwick.ac.uk'
