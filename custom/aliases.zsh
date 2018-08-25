de7bf34c15946fb87bc371378c2ca5e3c225a903
James Tattersall <10601770+jamerst@users.noreply.github.com>
2018-08-25 11:22:13 +0100

    Change format, various tweaks

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -4,6 +4,10 @@ alias chex='chmod u+x'
 alias ch644='chmod 644'
 alias ch755='chmod 755'
 
+# FZF
+alias preview="fzf --preview 'bat --color \"always\" {}'"
+export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(subl {})+abort'"
+
 # HOSTNAMES
 alias joshua='joshua.dcs.warwick.ac.uk'
 alias raw='radio.warwick.ac.uk'
@@ -12,10 +16,13 @@ alias raw='radio.warwick.ac.uk'
 alias l='exa -l --group-directories-first --time-style long-iso'
 alias la='exa -la --group-directories-first --time-style long-iso'
 
+# NCDU
+alias ncdu='ncdu --color dark'
+
 # SSH
 alias sshj='ssh u1708480@joshua.dcs.warwick.ac.uk'
 alias sshr='ssh jtattersall@radio.warwick.ac.uk'
 
 # ZSH CONFIG SHORTCUTS
 alias zshconf='subl ~/.zshrc'
-alias omz='xdg-open ~/.oh-my-zsh/custom'
+alias omz='xdg-open ~/.oh-my-zsh &'
